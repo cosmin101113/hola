78f0b3af3c0f29ede2168d7cd27c7085b3e808a8
Cosmin <tifcoosminmoise@gmail.com>
2023-12-13 18:37:48 +0100

    prueba

---
+++ papito.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+read -p "dame un numero" n1
+vble=1
+acumulador=0
+while [ $vble -le $n1 ];do
+    acumulador=$(($acumulador+$vble))
+    vble=$(($vble+1))
+    echo "$acumulador"
+done
+echo "$acumulador"
